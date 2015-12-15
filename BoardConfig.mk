206a392139632a702c532a9c9cefb42ecb0f12d7
vatsal <koradiavatsal@gmail.com>
2015-12-15 05:54:08 +0100

    Build kernel & rom with 5.2 uber
    Change-Id: I565e05ddf646bd951add43fddf76b65ba8d8f82e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -33,10 +33,10 @@ TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 TARGET_KERNEL_CONFIG := titan_defconfig
 
 #Kernel Toolchain
-KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-4.9-uber/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-eabi-5.2-uber/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
 # Rom Toolchain
-#TARGET_GCC_VERSION_EXP := 4.9-uber
+#TARGET_GCC_VERSION_EXP := 5.2-uber
 
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 10444800
