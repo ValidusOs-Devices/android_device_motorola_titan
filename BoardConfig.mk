203cd80c1ef9748f83805c5001bc1fc6ad438f00
vatsal <koradiavatsal@gmail.com>
2015-12-20 17:53:29 +0530

    Enable opts
    Change-Id: If2c1de9f5c8ce37f5111e413b6b1c68018a88829

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -29,6 +29,13 @@ TARGET_OTA_ASSERT_DEVICE := titan,titan_umts,titan_udstv,titan_umtsds,titan_reta
 # Board
 TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
 
+# Optimizations
+CLANG_O3 := true
+STRICT_ALIASING := false
+KRAIT_TUNINGS := false
+GRAPHITE_OPTS := false
+ENABLE_GCCONLY := true
+
 # Kernel
 TARGET_KERNEL_CONFIG := titan_defconfig
 
