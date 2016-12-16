81d2db4b97b3fdc5f07c2767ab713d493890668f
Vatsal <koradiavatsal@gmail.com>
2016-12-16 16:05:59 +0530

    Validus

--- validus.mk
+++ validus.mk
@@ -1,10 +1,10 @@
 $(call inherit-product, device/motorola/titan/full_titan.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/tesla/config/common_full_phone.mk)
-$(call inherit-product, vendor/tesla/config/caf_fw.mk)
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
+$(call inherit-product, vendor/validus/config/caf_fw.mk)
  
 PRODUCT_RELEASE_NAME := titan
-PRODUCT_NAME := tesla_titan
+PRODUCT_NAME := validus_titan
 
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
